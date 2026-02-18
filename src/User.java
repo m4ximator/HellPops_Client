bd3b96b0a57d1c4c9d29028f7b17129c253071f2
m4ximator <maxime.cherraf@gmail.com>
2026-02-18 15:07:20 +0100

    segmentation

---
+++ src/User.java
@@ -0,0 +1,37 @@
+package client;
+import commun.Jeton;
+import java.util.Scanner;
+
+public class User {
+	
+	private String username;
+	private String password;
+	private Jeton jeton;
+	private Scanner Scanner;
+	
+	
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public Jeton getJeton() {
+		return jeton;
+	}
+	public void setJeton(Jeton jeton) {
+		this.jeton = jeton;
+	}
+	
+	
+	
+	
+
+}
